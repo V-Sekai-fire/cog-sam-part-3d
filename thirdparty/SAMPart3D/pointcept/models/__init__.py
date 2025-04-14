ac72af8b4d514088cf368baf16e08be57b2742dd
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-04-13 19:38:11 -0700

    Remove unused modules and scripts from SAMPart3D

--- thirdparty/SAMPart3D/pointcept/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .builder import build_model
-
-from .SAMPart3D import SAMPart3D
-from .PTv3Object import PointTransformerV3Object
--- thirdparty/SAMPart3D/pointcept/models/builder.py
+++
@@ -1,9 +0,0 @@
-from pointcept.utils.registry import Registry
-
-MODELS = Registry("models")
-MODULES = Registry("modules")
-
-
-def build_model(cfg):
-    """Build models."""
-    return MODELS.build(cfg)
